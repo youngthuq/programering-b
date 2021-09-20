49904b12df8a53a64d3e698693205d95e2506261
youngthuq <77322025+youngthuq@users.noreply.github.com>
2021-09-20 13:24:32 +0100

    Det mega forvirende search bar

--- 9 java script araay/index.js
+++ 9 java script araay/index.js
@@ -1,5 +1,33 @@
-const sayHI = name => console.log('hi ' + name)
+let main = document.querySelector('main')
+let input = document.querySelector('#input')
 
-sayHI('Andres The Gay')
+const addCard = name => {
+let newCard = document.createElement('div')
+newCard.classList.add('card')
+let newH = document.createElement('h1')
+newH.innerHTML = name
+newCard.append(newH)
+main.append(newCard)
+}
 
-document.querySelector('body').addEventListener('click', e => console.log(e))
+addCard('Mælke-Mand')
+let names = ['Andreas', 'Theis', 'Frederik', 'Mie', 'Bjarke', 'Bjørn', 'Nagib', 'Sofus', 'Marius', 'Emil', 'Viktor', 'Lukas' ]
+
+names.push('Mælke Mand')
+
+
+names.map( name => addCard(name))
+
+input.addEventListener('input', () => {
+    let results = names.filter( name => name.toLowerCase().startsWith(input.value) )
+    main.innerHTML = ''
+    results.map( name => addCard(name) )
+} )
+
+
+
+
+//vi vil gerne lave et array
+//for(let i = 0; i < names.length; i++){
+//   console.log(names[i])
+//}
