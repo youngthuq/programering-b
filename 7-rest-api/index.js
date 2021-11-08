1bc7593235a1e186c00a67a9e25d7dee70600c32
youngthuq <77322025+youngthuq@users.noreply.github.com>
2021-11-08 12:54:43 +0100

    gg

--- 7-rest-api/index.js
+++ 7-rest-api/index.js
@@ -1,2 +1,8 @@
-fetch('/https://www.boredapi.com')
-    .then(Response => console.log(Response))
+let activity
+
+const getActibity = () => {
+    
+}
+fetch('https://www.boredapi.com/api/activity/')
+    .then( Response => Response.json() )
+    .then( json => {console.log(json)})
