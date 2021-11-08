002296ead809e42254be98a6569cd44395778a95
youngthuq <77322025+youngthuq@users.noreply.github.com>
2021-11-08 13:02:19 +0100

    gg

--- 7-rest-api/index.js
+++ 7-rest-api/index.js
@@ -1,8 +1,16 @@
 let activity
 
 const getActibity = () => {
-    
+    fetch('https://www.boredapi.com/api/activity/')
+        .then( Response => Response.json() )
+        .then( json => creatCard(activity) )
+
 }
-fetch('https://www.boredapi.com/api/activity/')
-    .then( Response => Response.json() )
-    .then( json => {console.log(json)})
+
+const creatCard = a => {
+    document.querySelector('#title').innerHTML = a.activity
+    document.querySelector('#participants').innerHTML = a.participants
+    document.querySelector('#acc').innerHTML = a.acc
+    document.querySelector('#price').innerHTML = a.price
+    document.querySelector('#cat').innerHTML = a.cat
+}
