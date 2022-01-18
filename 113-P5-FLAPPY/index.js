3e70015b165122dbeee002e58007a778a283f8ea
youngthuq <77322025+youngthuq@users.noreply.github.com>
2022-01-18 17:58:23 +0100

    gg

--- 113-P5-FLAPPY/index.js
+++ 113-P5-FLAPPY/index.js
@@ -6,21 +6,27 @@ let updrift = 25
 let rectX, rectY, rectW, rectH
 let rectSpeed = 10
 let score = 0
-let bird1, bird2, bird3 , bird4
+let fish1, fish2, fish3 , fish4, fish5, fish6, fish7, fish8, fish9
+const animRate = 50
 
 function preload() {
-    bird1 = loadImage('assets/bird1.jpg');
-    bird2 = loadImage('assets/bird2.jpg');
-    bird3 = loadImage('assets/bird3.jpg');
-    bird4 = loadImage('assets/bird4.jpg');
+    fish1 = loadImage('assets/fish1.png');
+    fish2 = loadImage('assets/fish2.png');
+    fish3 = loadImage('assets/fish3.png');
+    fish4 = loadImage('assets/fish4.png');
+    fish5 = loadImage('assets/fish5.png');
+    fish6 = loadImage('assets/fish6.png');
+    fish7 = loadImage('assets/fish7.png');
+    fish8 = loadImage('assets/fish8.png');
+    fish9 = loadImage('assets/fish9.png');
 }
 
 function setup(){
     createCanvas(windowWidth, windowHeight)
     frameRate(60)
-    background('green')
+    background('aquamarine')
     x = windowWidth/2
-    diameter = 32
+    diameter = 120
     y = diameter/2
     rectW = 20
     rectH = 100
@@ -43,7 +49,20 @@ function updateRect(){
 }
 
 function show(){
-    ellipse(x, y, diameter)
+    ellipseMode(CENTER)
+    imageMode(CENTER)
+    //ellipse(x, y, diameter)
+   
+    let a = frameCount % animRate / 10
+    if(a <= 1)image(fish1, x, y)
+    if(a >= 1 && a < 2)image(fish2, x, y)
+    if(a >= 2 && a < 3)image(fish3, x, y)
+    if(a >= 3 && a < 4)image(fish4, x, y)
+    if(a >= 4 && a < 5)image(fish4, x, y)
+    if(a >= 5 && a < 6)image(fish4, x, y)
+    if(a >= 6 && a < 7)image(fish4, x, y)
+    if(a >= 7 && a < 8)image(fish4, x, y)
+    if(a <= 9)image(fish9)
 }
 
 function update(){
@@ -55,15 +74,29 @@ function update(){
         y = windowHeight - diameter/2
         velocity = -velocity
     }
+    if(y < diameter/2){
+        y = diameter/2
+        score = score + 4
+    }
 }
 
+function collission(){
+    if(x > rectX && x < rectX + rectW){
+        if(y < rectH || y > windowHeight - rectH){
+            score -= 1
+        }
+    }
+}
+
+
 function draw(){
-    background('green')
+    background('aquamarine')
     show()
     update()
     showRect()
     updateRect()
     select('#info').html(score)
+    collission()
 }
 
 function keyPressed(key){
