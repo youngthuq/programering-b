cf39c3eccbc8d88ca240f47dc0370bea392af67f
youngthuq <77322025+youngthuq@users.noreply.github.com>
2022-02-01 13:37:23 +0100

    gg

--- p5 objekts/components/Ball.js
+++ p5 objekts/components/Ball.js
@@ -1,13 +1,32 @@
 class Ball {
     constructor(radius, xpos, ypos, col){
-        thhis.radius = radius 
+        this.radius = radius 
         this.xpos = xpos
         this.ypos = ypos
         this.col = col
+
+        this.gravity = 1
+        this.friction = .97
+        this.velocity = 0
+        this.updrift = 25
     }
 
     show(){
         fill(this.col)
         ellipse(this.xpos, this.ypos, this.radius)
+    }
+
+    update(){
+        this.velocity += this.gravity
+        this.velocity *= this.friction
+        this.ypos += this.velocity
+
+        if(this.ypos > height - this.radius){
+            this.ypos = height - this.radius
+            this.velocity = - this.velocity
+        }
+    }
+    up(){
+        this.velocity -= this.updrift
     }
 }
--- p5 objekts/index.js
+++ p5 objekts/index.js
@@ -1,5 +1,17 @@
-function setup(){
-createCanvas(400, 400)
-background('purple')
+let viktor, lukas
 
+function setup(){
+    createCanvas(400, 400)
+    background('purple')
+
+    let viktor = new Ball(12, 200, 100, 'green')
+    let lukas = new Ball(8, 120, 70, 'gray')
+    console.log(viktor)
+    console.log(lukas)
+}
+
+function draw(){
+    background()
+    viktor.show()
+    lukas.show()
 }
